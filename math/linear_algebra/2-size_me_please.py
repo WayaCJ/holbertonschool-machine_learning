98b2d9cbde4f4596f160069ea0692dd29feb3cda
CJ <sotocarlos1956@gmail.com>
2025-04-28 14:25:11 -0400

    fixing

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,6 +3,7 @@
 
 
 def matrix_shape(matrix):
+    """Calculates the shape of a matrix"""
     if isinstance(matrix, list) and matrix:
         if isinstance(matrix[0], list):
             return [len(matrix)] + matrix_shape(matrix[0])
