96427b9a1c54d1eaa6bdf756bdc1039834b97f7d
CJ <sotocarlos1956@gmail.com>
2025-04-28 14:32:36 -0400

    fixing

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,7 +6,7 @@ def add_matrices2D(mat1, mat2):
     """Adds two matrices element-wise"""
     if len(mat1) != len(mat2) or any(len(row1) != len(row2) for row1,
         row2 in zip(mat1, mat2)):
-        return None
+            return None
 
     return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[i]))]
             for i in range(len(mat1))]
