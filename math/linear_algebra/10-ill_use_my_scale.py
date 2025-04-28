e46676b3cfea2e27f31daef95dce7dd78845bd19
CJ <sotocarlos1956@gmail.com>
2025-04-28 15:33:04 -0400

    fixing

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -2,21 +2,9 @@
 
 def np_shape(matrix):
     """
-    Calculates the shape of a 2D matrix (list of lists).
-    
-    Args:
-        matrix (list): A 2D matrix represented as a list of lists.
-    
-    Returns:
-        tuple: The shape of the matrix (number of rows, number of columns).
+    Returns the shape of a matrix
     """
-    if not matrix:  # If the matrix is empty
-        return (0,)
-
-    # Number of rows (outer list length)
-    num_rows = len(matrix)
-    
-    # Number of columns (assuming all rows are of equal length)
-    num_cols = len(matrix[0]) if matrix else 0
-    
-    return (num_rows, num_cols)
+    try:
+        return (len(matrix),) + np_shape(matrix[0])
+    except TypeError:
+        return (len(matrix),)
