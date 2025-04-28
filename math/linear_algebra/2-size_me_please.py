9f6507efa27ccb8fe7e108a083902c471e6569ff
CJ <sotocarlos1956@gmail.com>
2025-04-28 14:16:40 -0400

    fixing

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,5 +5,5 @@ def matrix_shape(matrix):
         if isinstance(matrix[0], list):
             return [len(matrix)] + matrix_shape(matrix[0])
         else:
-            return [len(matrix)]    
+            return [len(matrix)]
     return []
