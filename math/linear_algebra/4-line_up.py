d4a66e9c649dcd5684bd730a3b4768d8e26f6408
CJ <sotocarlos1956@gmail.com>
2025-04-28 14:26:47 -0400

    fixing

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Module that adds"""
+
 
 def add_arrays(arr1, arr2):
+    """Adds two arrays element-wise"""
     if len(arr1) != len(arr2):  # Check if arrays are not of the same length
         return None
 
