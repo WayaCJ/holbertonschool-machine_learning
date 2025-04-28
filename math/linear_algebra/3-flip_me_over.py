dbd598f181621a41845a7bf6ef572e86315d4005
CJ <sotocarlos1956@gmail.com>
2025-04-28 14:24:13 -0400

    fixing

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Module that returns transpose"""
+
 
 def matrix_transpose(matrix):
+    """Returns the transpose of a 2D matrix"""
     return [list(row) for row in zip(*matrix)]
